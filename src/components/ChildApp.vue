6e77de9f0849873051b953a07db0e26d3019b012
Abrar Ahmed <abrar.gsoc@gmail.com>
2019-08-26 16:47:54 +0500

    vue parent to child comm practice

---
+++ src/components/ChildApp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h3>Child Component</h3>
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  //   what if we want some calculation on data change.then use watch
+  props: {
+    name: {
+      default: "dummyName"
+    }
+  },
+
+  watch: {
+    name: {
+      // the callback will be called immediately after the start of the observation
+      immediate: true,
+      handler(val, oldVal) {
+        console.log(val);
+        console.log(oldVal);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  color: brown;
+}
+</style>
